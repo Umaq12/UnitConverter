efa8d78d02a4eedcd4a060707b30b99c4730841f
Umaq <feratj01@gmail.com>
2024-03-16 00:40:16 +0300

    Day 6 - Converter

--- app/src/main/java/ms/umaq/unitconverter/MainActivity.kt
+++ app/src/main/java/ms/umaq/unitconverter/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -36,7 +37,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ms.umaq.unitconverter.ui.theme.ShoppingListApp
 import ms.umaq.unitconverter.ui.theme.UnitConverterTheme
+import java.nio.file.WatchEvent
 import kotlin.math.roundToInt
 
 
@@ -50,123 +53,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    UnitConverter()
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+) {
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun UnitConverter() {
-    var inputValue by remember { mutableStateOf("") }
-    var outputValue by remember { mutableStateOf("") }
-    var inputUnit by remember { mutableStateOf("Centimeters") }
-    var outputUnity by remember { mutableStateOf("Meters") }
-    var iExpanded by remember { mutableStateOf(false) }
-    var oExpanded by remember { mutableStateOf(false) }
-    var conversionFactor = remember { mutableStateOf(1.0) }
-    var oConversionFactor = remember { mutableStateOf(0.01) }
-    fun convertUnits() {
-        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
-        val result = (inputValueDouble * conversionFactor.value * 100.0 / oConversionFactor.value).roundToInt() / 100.0
-        outputValue = result.toString()
-    }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Unit Converter", modifier = Modifier.padding(100.dp))
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = inputValue, onValueChange = {
-            inputValue = it
-            convertUnits()
-        }, label = { Text(text = "Enter value") })
-        Spacer(modifier = Modifier.height(16.dp))
-        Row {
-            Box {
-                Button(onClick = { iExpanded = true }) {
-                    Text(text = inputUnit)
-                    Icon(Icons.Default.ArrowDropDown, contentDescription = null)
-                }
-                DropdownMenu(expanded = iExpanded, onDismissRequest = { iExpanded = false }) {
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Centimeters"
-                        conversionFactor.value = 0.01
-                        convertUnits()
-                    })
-                    DropdownMenuItem(text = { Text("Meters") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Meters"
-                        conversionFactor.value = 1.0
-                        convertUnits()
-                    })
-                    DropdownMenuItem(text = { Text("Feet") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Feet"
-                        conversionFactor.value = 0.3048
-                        convertUnits()
-                    })
-                    DropdownMenuItem(text = { Text("Milimeters") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Milimeters"
-                        conversionFactor.value = 0.001
-                        convertUnits()
-                    })
-                }
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            Box {
-                Button(onClick = { oExpanded = true }) {
-                    Text(text = outputUnity)
-                    Icon(Icons.Default.ArrowDropDown, contentDescription = null)
-                }
-                DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded = false }) {
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Centimeters"
-                        oConversionFactor.value = 0.01
-                        convertUnits()
-                    })
-                    DropdownMenuItem(text = { Text("Meters") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Meters"
-                        oConversionFactor.value = 1.0
-                        convertUnits()
-                    })
-                    DropdownMenuItem(text = { Text("Feet") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Feet"
-                        oConversionFactor.value = 0.3048
-                        convertUnits()
-                    })
-                    DropdownMenuItem(text = { Text("Milimeters") }, onClick = {
-                        iExpanded = false
-                        inputUnit = "Milimeters"
-                        oConversionFactor.value = 0.001
-                        convertUnits()
-                    })
-                }
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Result:")
-    }
-
-    @Composable
-    fun unitConverterPreview() {
-        UnitConverter()
-
-    }
 }
