2b40370386f9fcac8f8157b815cc51d055226353
Umaq <feratj01@gmail.com>
2024-03-20 18:31:16 +0300

    Day 6 - Converter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,16 @@ android {
 }
 
 dependencies {
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    // Делать запросы
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Json to kotlin
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Загрузка избражений
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
